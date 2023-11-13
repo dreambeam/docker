37e9e71055bc86c54bb7b53a03883db8fc0ec69f
Bleza Takouda <20278294+dreambeam@users.noreply.github.com>
2023-11-13 14:51:59 +0900

    Create deploys.sh
    Add deployment file

---
+++ mssql/deploys.sh
@@ -0,0 +1,4 @@
+docker run -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=YourStrong!Passw0rd' \
+   --name 'sql1' -p 1401:1433 \
+   -v sql1data:/var/opt/mssql \
+   -d mcr.microsoft.com/mssql/server:2022-latest
